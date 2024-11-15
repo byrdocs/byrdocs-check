ef9c3ac0b042f614effb041494016992e4f83c56
liulifox233 <liulifox@qq.com>
2024-11-15 23:49:37 +0800

    Update

--- check-cmt/src/metadata.rs
+++ check-cmt/src/metadata.rs
@@ -1,4 +1,4 @@
-use serde::{Deserialize, Serialize};
+use serde::{ser::SerializeStruct, Deserialize, Serialize};
 
 #[derive(serde::Deserialize, Debug, Serialize)]
 pub enum Type {
@@ -17,12 +17,11 @@ pub enum Data {
     Doc(Doc),
 }
 
-#[derive(Debug, Serialize)]
+#[derive(Debug)]
 #[allow(dead_code)]
 pub struct MetaData {
     pub id: String,
     pub url: String,
-    #[serde(rename = "type")]
     pub type_: Type,
     pub data: Data,
 }
@@ -39,8 +38,6 @@ pub struct Test {
     pub filesize: Option<u64>,
 }
 
-
-
 #[derive(serde::Deserialize, Debug, Serialize)]
 #[allow(dead_code)]
 pub struct Time {
@@ -49,7 +46,6 @@ pub struct Time {
     pub semester: Option<String>,
     pub stage: Option<String>,
 }
-
 
 #[derive(serde::Deserialize, Debug, Serialize)]
 #[allow(dead_code)]
@@ -89,16 +85,56 @@ impl<'de> Deserialize<'de> for MetaData {
     {
         let value = serde_yaml::Value::deserialize(deserializer)?;
         let data = match value.get("type").and_then(|t| t.as_str()) {
-            Some("test") => Ok(Data::Test(serde_yaml::from_value(value.get("data").unwrap().clone()).map_err(serde::de::Error::custom)?)),
-            Some("book") => Ok(Data::Book(serde_yaml::from_value(value.get("data").unwrap().clone()).map_err(serde::de::Error::custom)?)),
-            Some("doc") => Ok(Data::Doc(serde_yaml::from_value(value.get("data").unwrap().clone()).map_err(serde::de::Error::custom)?)),
+            Some("test") => Ok(Data::Test(
+                serde_yaml::from_value(value.get("data").unwrap().clone())
+                    .map_err(serde::de::Error::custom)?,
+            )),
+            Some("book") => Ok(Data::Book(
+                serde_yaml::from_value(value.get("data").unwrap().clone())
+                    .map_err(serde::de::Error::custom)?,
+            )),
+            Some("doc") => Ok(Data::Doc(
+                serde_yaml::from_value(value.get("data").unwrap().clone())
+                    .map_err(serde::de::Error::custom)?,
+            )),
             _ => Err(serde::de::Error::custom("unknown type")),
         }?;
         Ok(MetaData {
-            id: value["id"].as_str().ok_or_else(|| serde::de::Error::custom("missing id"))?.to_owned(),
-            url: value["url"].as_str().ok_or_else(|| serde::de::Error::custom("missing url"))?.to_owned(),
-            type_: serde_yaml::from_value(value["type"].clone()).map_err(serde::de::Error::custom)?,
+            id: value["id"]
+                .as_str()
+                .ok_or_else(|| serde::de::Error::custom("missing id"))?
+                .to_owned(),
+            url: value["url"]
+                .as_str()
+                .ok_or_else(|| serde::de::Error::custom("missing url"))?
+                .to_owned(),
+            type_: serde_yaml::from_value(value["type"].clone())
+                .map_err(serde::de::Error::custom)?,
             data,
         })
+    }
+}
+
+impl Serialize for MetaData {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        let mut state = serializer.serialize_struct("MetaData", 4)?;
+        state.serialize_field("id", &self.id)?;
+        state.serialize_field("url", &self.url)?;
+        state.serialize_field("type", &self.type_)?;
+        match &self.data {
+            Data::Test(test) => {
+                state.serialize_field("data", &test)?;
+            }
+            Data::Book(book) => {
+                state.serialize_field("data", &book)?;
+            }
+            Data::Doc(doc) => {
+                state.serialize_field("data", &doc)?;
+            }
+        }
+        state.end()
     }
 }
